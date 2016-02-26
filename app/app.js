4e3a7f97c14782788722d71f944882c49562a262
a-ponomarenko <alexxxponomarenko@gmail.com>
2016-02-26 11:55:52 +0200

    created files for pages auth and jokes

--- app/app.js
+++ app/app.js
@@ -2,12 +2,13 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  //'ngRoute',
-  'ui.router',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+    // 'ngRoute',
+    'ui.router',
+    'myApp.jokes',
+    'myApp.view2',
+    'myApp.auth',
+    'myApp.version'
+])
+    //.config(['$routeProvider', function($routeProvider) {
+    //    $routeProvider.otherwise({redirectTo: '/view1'});
+    //}]);
---
+++ app/view_jokes/jokes.js
@@ -0,0 +1,18 @@
+angular.module('myApp.jokes', [])
+
+    .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('jokes', {
+                url: '/jokes',
+                views: {
+                    'jokesContent': {
+                        templateUrl: "view_jokes/jokes.html",
+                        controller: 'JokesCtrl as jokes'
+                    }
+                }
+            })
+    }])
+
+    .controller('JokesCtrl', ['$rootScope', function($rootScope){
+
+    }])
